761757d6b4d65ec426236531e3949974e42f37da
mhormigo <m.hormigo@alumnos.upm.es>
2020-06-03 14:03:39 +0200

    Corrections issue#2

--- src/screens/main/tab/Home.js
+++ src/screens/main/tab/Home.js
@@ -4,14 +4,16 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 import Post from '../../../components/Post'
 import clipsList from '../../../data/clipsList'
-import Styles from '../../../constants/Styles'
+import { colors, layout } from '../../../constants/Styles'
 
 export default class Home extends React.Component {
 
     render() {
         return (
-            <SafeAreaView style={{flex: 1, marginHorizontal: 22, backgroundColor: '#fff'}}>
-                <Post />
+            <SafeAreaView style={{flex: 1, backgroundColor: colors.background}}>
+                <View style={{flex: 1, paddingHorizontal: layout.paddingHorizontal}}>
+                    <Post />
+                </View>
             </SafeAreaView>
         ); 
     }
