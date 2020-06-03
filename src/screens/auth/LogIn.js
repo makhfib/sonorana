372fa08a639537279504920dd28ab0a7f5894b8c
Mohammed Makhfi Boulaich <accounts@makhfib.com>
2020-06-03 10:43:43 +0200

    Added brand assets and login background

--- src/screens/auth/LogIn.js
+++ src/screens/auth/LogIn.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ImageBackground } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { colors } from '../../constants/Styles'
 
 export default class LogIn extends Component {
     render() {
         return (
-            <SafeAreaView>
-                <Text> Log In </Text>
+            <SafeAreaView style={{
+                flex: 1,
+                backgroundColor: colors.lightgray
+            }}>
+                <ImageBackground 
+                    style={{
+                        flex: 1,
+                        backgroundColor: 'green',
+                    }}
+                    source={require('../../assets/images/linear-background.png')}
+                >
+                    <Text> Log In </Text>
+                    
+                </ImageBackground>
             </SafeAreaView>
         )
     }
