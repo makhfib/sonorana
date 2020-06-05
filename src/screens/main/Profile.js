1acb2a9a43e90cd561236b0e5344ac9cc924411a
Mohammed Makhfi Boulaich <accounts@makhfib.com>
2020-06-05 17:07:57 +0200

    Separated styles from components and cleaned code

--- src/screens/main/Profile.js
+++ src/screens/main/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, View, ImageBackground, Image } from 'react-native'
+import { Text, View, ImageBackground, Image, StyleSheet } from 'react-native'
 import { colors, layout } from '../../constants/Styles'
 import NavigationBar from '../../components/NavigationBar'
 import ActionButton from '../../components/ActionButton'
@@ -37,114 +37,47 @@ export default class Profile extends Component {
                 />
                 <Separator />
                 <View
-                    style={{
-                        height: 90 + 75 / 2,
-                        justifyContent: 'flex-end',
-                        paddingHorizontal: layout.paddingHorizontal,
-                        backgroundColor: colors.background
-                    }}
+                    style={styles.images}
                 >
                     <ImageBackground
-                        style={{
-                            top: 0,
-                            position: 'absolute',
-                            height: 90,
-                            width: layout.deviceWidth,
-                            backgroundColor: colors.background
-                        }}
+                        style={styles.profileHeader}
                         source={{ uri: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hdwallpaper.nu%2Fwp-content%2Fuploads%2F2017%2F03%2Fvalley-16.jpg&f=1&nofb=1' }}
                     >
                     </ImageBackground>
                     <View
-                        style={{
-                            height: 75,
-                            width: 75,
-                            borderRadius: 75 / 2,
-                            backgroundColor: colors.background,
-                            justifyContent: 'center',
-                            alignItems: 'center'
-                        }}
+                        style={styles.photoContainer}
                     >
                         <Image
-                            style={{
-                                height: 70,
-                                width: 70,
-                                borderRadius: 75 / 2,
-                                backgroundColor: colors.background,
-                            }}
+                            style={styles.profilePhoto}
                             source={{ uri: 'https://media-exp1.licdn.com/dms/image/C4D03AQEy3CR_2OAw6Q/profile-displayphoto-shrink_200_200/0?e=1596672000&v=beta&t=_YkGI9DUd9QiWFE3xLpVcif1KqOsaHnvZxmgU8WxUDw' }}
                         />
                     </View>
                 </View>
-                <View
-                    style={{
-                        paddingHorizontal: layout.paddingHorizontal,
-                        paddingVertical: layout.paddingHorizontal / 2,
-                        backgroundColor: colors.background
-                    }}
-                >
-                    <Text
-                        style={{
-                            fontWeight: 'bold'
-                        }}
-                    >
+                <View style={styles.basicInfoContainer}>
+                    <Text style={{ fontWeight: 'bold' }}>
                         Mohammed Makhfi
                     </Text>
-                    <Text
-                        style={{
-                            marginVertical: 10,
-                        }}
-                    >
+                    <Text style={{ marginVertical: 10, }}>
                         {'Currently building the future. Chairman of Sonorana'}
                     </Text>
-                    <Text
-                        style={{
-                            color: colors.blue
-                        }}
-                    >
+                    <Text style={{ color: colors.blue }}>
                         {'makhfib.com'}
                     </Text>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            marginVertical: 10,
-                        }}
-                    >
-                        <Text
-                            style={{
-                                fontWeight: 'bold'
-                            }}
-                        >
+                    <View style={{ flexDirection: 'row', marginVertical: 10, }}>
+                        <Text style={{ fontWeight: 'bold' }}>
                             {'31'}
                         </Text>
-                        <Text
-                            style={{
-                                marginRight: 10,
-                            }}
-                        >
+                        <Text style={{ marginRight: 10, }} >
                             {' following'}
                         </Text>
-                        <Text
-                            style={{
-                                fontWeight: 'bold'
-                            }}
-                        >
+                        <Text style={{ fontWeight: 'bold' }} >
                             {'158'}
                         </Text>
-                        <Text
-                            style={{
-                                
-                            }}
-                        >
+                        <Text style={{  }} >
                             {' following'}
                         </Text>
                     </View>
-                    <View
-                        style={{
-                            flexDirection: 'row',
-                            marginVertical: 10,
-                        }}
-                    >
+                    <View style={styles.buttonsContainer}>
                         <ActionButton
                             icon={require('../../assets/icons/edit.png')}
                             title={'Edit profile'}
@@ -176,3 +109,46 @@ export default class Profile extends Component {
         )
     }
 }
+
+const profileHeaderHeight = 90
+const profilePhotoHeight = 75
+
+const styles = StyleSheet.create({
+    images: {
+        height: profileHeaderHeight + profilePhotoHeight / 2,
+        justifyContent: 'flex-end',
+        paddingHorizontal: layout.paddingHorizontal,
+        backgroundColor: colors.background
+    },
+    profileHeader: {
+        top: 0,
+        position: 'absolute',
+        height: profileHeaderHeight,
+        width: layout.deviceWidth,
+        backgroundColor: colors.background,
+        resizeMode: 'cover'
+    },
+    photoContainer: {
+        height: profilePhotoHeight,
+        width: profilePhotoHeight,
+        borderRadius: profilePhotoHeight / 2,
+        backgroundColor: colors.background,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    profilePhoto: {
+        height: profilePhotoHeight-5,
+        width: profilePhotoHeight-5,
+        borderRadius: profilePhotoHeight / 2,
+        backgroundColor: colors.background,
+    },
+    basicInfoContainer: {
+        paddingHorizontal: layout.paddingHorizontal,
+        paddingVertical: layout.paddingHorizontal / 2,
+        backgroundColor: colors.background
+    },
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginVertical: 10,
+    }
+})
